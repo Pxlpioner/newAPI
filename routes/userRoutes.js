04d4169d24b421464cc2a7086c3aae1dcaf5a069
crislk <quybpps40589@gmail.com>
2024-11-30 05:28:20 +0700

    okila

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,23 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const userModel = require("../models/userModel");
+const JWT = require('jsonwebtoken');
+const tokenConfig = require("../configs/tokenConfig");
+
+router.post("/login", async (req, res) => {
+    try {
+        const { inputName, inputPass } = req.body;
+        const item = await userModel.findOne({ username: inputName, password: inputPass });
+
+        if (!item) { return res.status(404).json({"status": false, "message": 'Username and password are incorrect' }); }
+
+        const token = JWT.sign({ username: inputName }, tokenConfig.SECRETKEY, { expiresIn: '30s' });
+        const refreshToken = JWT.sign({ username: inputName }, tokenConfig.SECRETKEY, { expiresIn: '1h' })
+
+        return res.status(200).json({"status": true, "token": token, "refreshToken": refreshToken });
+
+    } catch (e) { return res.status(400).json({ message: e.message }); }
 });
 
 module.exports = router;
