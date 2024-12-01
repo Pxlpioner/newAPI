f6e373bd5cda0440bfdbfefc892a06548f5a7975
crislk <quybpps40589@gmail.com>
2024-12-01 13:28:44 +0700

    oke at 13:28 PM

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -17,6 +17,7 @@ router.post("/getAll", async (req, res) => {
             const mlist = await productModel.find();
             res.status(200).json(mlist); 
         });
+        
     } catch (e) { res.status(400).json({ message: `Something went wrong - ${e.message}` }); }
 });
 
@@ -36,11 +37,17 @@ router.get('/getByid/:id', async (req, res) => {
 
 router.put('/updateByid/', async (req, res) => {
     try {
-        const {id} = req.query;
+        const token = req.header("Authorization").split(" ")[1];
+        if (!token){ return res.status(401).json({message: "401 - Bad Request" }); }
+
+        const {id, name, price } = req.query;
         const updateItem = productModel.findById(id);
-        if (!updateItem) { return res.status(401).json({"status": 403}); }
-        
+        if (updateItem == null) { return res.status(401).json({"status": 403}); }
+
+        updateItem.productName = name ? name : updateItem.productName;
+        updateItem.productPrice = price ? price : updateItem.v;
         await productModel.save(updateItem);
+        
         res.status(200).json({"updateItem": updateItem});
 
     } catch (e) { res.status(400).json({ message: `Something went wrong - ${e.message}` }); }
